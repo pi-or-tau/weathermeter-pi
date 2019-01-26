efc63ef24b745d4b6e83959760f307d6eed07db2
pi-or-tau <hcollin3.14@gmail.com>
2019-01-25 22:58:14 -0500

    Updates

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import assigner
+import sun_time
 
+
+sun = sun_time.sun()
 current = assigner.vari_init()
 
 print('Temperature: ' + str(current.temp_F) + 'F')
@@ -7,3 +10,5 @@ print('Weather: ' + current.weather_description)
 print('Sunrise: ' + current.sunrise)
 print('Sunset: ' + current.sunset)
 print('At: ' + current.time_taken)
+
+#print(sun.text2int(current.sunset[0]))
